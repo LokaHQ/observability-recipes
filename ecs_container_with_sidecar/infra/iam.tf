fe88e89c3dc7382101a25ea793919577b0e648ec
= <=>
2025-09-03 13:23:14 +0200

    extended IAM permissions with a role for the new lambda function

--- ecs_container_with_sidecar/infra/iam.tf
+++ ecs_container_with_sidecar/infra/iam.tf
@@ -14,9 +14,9 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
-resource "aws_iam_policy" "ecs_ssm_read" {
-  name        = "ecs-ssm-read-policy"
-  description = "Allow ECS tasks to read SSM parameters for OTEL"
+resource "aws_iam_policy" "ecs_ssm_read_sqs_write" {
+  name        = "ecs-ssm-read-sqs-write-policy"
+  description = "Allow ECS tasks to read SSM parameters for OTEL and send SQS messages"
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -29,8 +29,13 @@ resource "aws_iam_policy" "ecs_ssm_read" {
           "ssm:GetParametersByPath"
         ],
         Resource = aws_ssm_parameter.otel_config.arn
-      }
-    ]
+      },
+      {
+        Effect   = "Allow",
+        Action   = ["sqs:SendMessage"],
+        Resource = "*"
+
+    }]
   })
 }
 
@@ -41,7 +46,7 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_attach" {
 
 resource "aws_iam_role_policy_attachment" "ecs_task_ssm_attach" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = aws_iam_policy.ecs_ssm_read.arn
+  policy_arn = aws_iam_policy.ecs_ssm_read_sqs_write.arn
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_xray_attach" {
@@ -53,3 +58,51 @@ resource "aws_iam_role_policy_attachment" "ecs_task_cloudwatch_attach" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
+
+resource "aws_iam_role" "lambda_consumer_role" {
+  name = "lambda_consumer_example_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "sqs_consumer_lambda_policy" {
+  name = "sqs_consumer_example_lambda_policy"
+  role = aws_iam_role.lambda_consumer_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = ["sqs:ReceiveMessage", "sqs:DeleteMessage", "sqs:GetQueueAttributes"],
+      Resource = aws_sqs_queue.standard_queue.arn
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "xray:PutTraceSegments",
+          "xray:PutTelemetryRecords"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
