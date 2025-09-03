29c3e3c971e349941e0edd85cb23797339cd4afc
= <=>
2025-09-03 14:52:39 +0200

    instrumentation code and an additonal endpoint

--- ecs_container_with_sidecar/app/main.py
+++ ecs_container_with_sidecar/app/main.py
@@ -1,34 +1,64 @@
 from fastapi import FastAPI
-from opentelemetry import trace
+from opentelemetry import trace, metrics
 from opentelemetry.sdk.resources import Resource, SERVICE_NAME
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.extension.aws.trace import AwsXRayIdGenerator
+from opentelemetry.instrumentation.botocore import BotocoreInstrumentor
+import boto3
+import json
+import random
+import os
+from typing import Dict
+
+sqs_client = boto3.client("sqs")
 
 app = FastAPI()
 
 # Define resource with service name
-resource = Resource.create({
-    SERVICE_NAME: "my-fastapi-app"
-})
+resource = Resource.create({SERVICE_NAME: "my-fastapi-app"})
 
-# Set up tracer provider
-provider = TracerProvider(resource=resource)
 
 # Configure OTLP exporter to talk to the sidecar
-otlp_exporter = OTLPSpanExporter(
-    endpoint="http://localhost:4317",
-    insecure=True
-)
-provider.add_span_processor(BatchSpanProcessor(otlp_exporter))
+# It will send traces in OTLP format to the collector running on port 4317
+otlp_exporter = OTLPSpanExporter(endpoint="http://localhost:4317", insecure=True)
 
-# Set global provider
-trace.set_tracer_provider(provider)
+
+span_processor = BatchSpanProcessor(otlp_exporter)
+trace.set_tracer_provider(
+    TracerProvider(
+        resource=resource,
+        active_span_processor=span_processor,
+        id_generator=AwsXRayIdGenerator(),
+    )
+)
+
+
+## Setup metrics export
+metric_exporter = OTLPMetricExporter()
+metric_reader = PeriodicExportingMetricReader(exporter=metric_exporter)
+metric_provider = MeterProvider(metric_readers=[metric_reader])
+metrics.set_meter_provider(metric_provider)
+
 
 # Instrument FastAPI
 FastAPIInstrumentor.instrument_app(app)
+BotocoreInstrumentor().instrument()
+
 
 @app.get("/health-check")
 def index():
     return {"status": "OK"}
+
+
+@app.post("/messages")
+def message(payload: Dict[str, str]):
+    sqs_client.send_message(
+        QueueUrl=os.environ["QUEUE_URL"], MessageBody=json.dumps(payload)
+    )
+    return {"status": "SENT"}
