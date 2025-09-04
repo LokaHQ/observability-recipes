2262bdf438f44ca7ac1144d41b4c288d57619896
= <=>
2025-09-04 19:35:28 +0200

    manual context propagation

--- ecs_container_with_sidecar/app/consumer/lambda_function.py
+++ ecs_container_with_sidecar/app/consumer/lambda_function.py
@@ -1,5 +1,7 @@
 from opentelemetry import trace
-import json
+from opentelemetry.baggage import get_baggage
+from opentelemetry.baggage.propagation import W3CBaggagePropagator
+from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 
 
 tracer = trace.get_tracer(__name__)
@@ -7,7 +9,28 @@ tracer = trace.get_tracer(__name__)
 
 @tracer.start_as_current_span("consume_messages")
 def lambda_handler(event, context):
+    for record in event["Records"]:
+        # extract message attributes
+        msg_attrs = {
+            k: v["stringValue"]
+            for k, v in record["messageAttributes"].items()
+        }
+
+
+        ctx = TraceContextTextMapPropagator().extract(msg_attrs)
+        ctx = W3CBaggagePropagator().extract(msg_attrs, context=ctx)
+
+        user_id = get_baggage("user_id", context=ctx)
+
+        with tracer.start_as_current_span("sqs_processor_lambda", context=ctx):
+            print(f"Processing message for user: {user_id}")
+
+    
     return {
         "statusCode": 200,
         "body": "Message read"
 }
+
+
+
+
--- ecs_container_with_sidecar/app/main.py
+++ ecs_container_with_sidecar/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from opentelemetry import trace, metrics
+from opentelemetry import trace, metrics, baggage, context
 from opentelemetry.sdk.resources import Resource, SERVICE_NAME
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
@@ -10,10 +10,13 @@ from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.sdk.extension.aws.trace import AwsXRayIdGenerator
 from opentelemetry.instrumentation.botocore import BotocoreInstrumentor
+from opentelemetry.baggage.propagation import W3CBaggagePropagator
+from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 import boto3
 import json
 import os
 from typing import Dict
+from uuid import uuid4
 
 sqs_client = boto3.client("sqs")
 
@@ -49,6 +52,8 @@ metrics.set_meter_provider(metric_provider)
 FastAPIInstrumentor.instrument_app(app)
 BotocoreInstrumentor().instrument()
 
+tracer = trace.get_tracer(__name__)
+
 
 @app.get("/health-check")
 def index():
@@ -57,7 +62,22 @@ def index():
 
 @app.post("/messages")
 def message(payload: Dict[str, str]):
-    sqs_client.send_message(
-        QueueUrl=os.environ["QUEUE_URL"], MessageBody=json.dumps(payload)
-    )
-    return {"status": "SENT"}
+    # let's simulate a scenario where we're propagating the ID of the logged in user
+    ctx = baggage.set_baggage("user_id", str(uuid4()))
+
+    message_attributes = {}
+    TraceContextTextMapPropagator().inject(message_attributes, context=ctx)
+    W3CBaggagePropagator().inject(message_attributes, context=ctx)
+
+    sqs_attributes = {
+        k: {"StringValue": v, "DataType": "String"}
+        for k, v in message_attributes.items()
+    }
+
+    with tracer.start_as_current_span("send_sqs_message"):
+        sqs_client.send_message(
+            QueueUrl=os.environ["QUEUE_URL"],
+            MessageBody=json.dumps(payload),
+            MessageAttributes=sqs_attributes,
+        )
+        return {"status": "SENT"}
